a953986fe60e7014972ea54cdbc8bfafff6d3811
Kyle P. Edwards <businesskylepedwards@outlook.com>
2025-05-17 07:49:29 -0500

    Updated

--- javascript_section/js/script.js
+++ javascript_section/js/script.js
@@ -1,1 +1,24 @@
 console.log("\n\nHello World!\n\n")
+
+// Variables, Lets, and Consts
+var this_is_a_variable = "This is a variable.";
+let this_is_a_let = "This is a let.";
+const this_is_a_const = "This is a const.";
+
+// This is a single line comment.
+/* 
+    This is a multi-line comment.
+*/
+
+// Data Structures
+var this_is_a_string = "String";
+var this_is_a_number = 3;
+var this_is_a_float = 3.14;
+var this_is_a_bool =  false;
+var this_is_a_list = [1, "2", 3, 4.0, [5]];
+var this_is_a_null = null;
+var this_is_a_notDefined = undefined;
+var this_is_a_function = function() { return 33; };
+var obj = {
+    'name': "Kyle"
+}
