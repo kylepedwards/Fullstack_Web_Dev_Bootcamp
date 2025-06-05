18b6c4d4f6aecfedf6d0f4a707f1c5dbf207daa4
Kyle P. Edwards <businesskylepedwards@outlook.com>
2025-06-05 13:54:45 -0500

    Updated HTML, JavaScript, and README files

--- Front_End/html_css_and_js/js/script.js
+++ Front_End/html_css_and_js/js/script.js
@@ -22,3 +22,22 @@ var getAge = function() { return 31; }; // function in a variabl
 var obj = {
     'key': 'value pair'
 }; 
+
+// Conditionals
+if (typeof(str) == String) {
+    console.log("Correct! This is a string.")
+} else {
+    console.log("Not a string.")
+};
+
+if (num >= 0) {
+    console.log("2 is greater than 0.")
+} else {
+    console.log("Number is not greater than 0.")
+};
+
+// DOM manipulation
+var domDummyManipulator = document.getElementById("dummy");
+domDummyManipulator.innerText = "This text came from 'innerText' JavaScript.";
+domDummyManipulator.innerHTML = "This text came from 'innerHTML' JavaScript."
+
