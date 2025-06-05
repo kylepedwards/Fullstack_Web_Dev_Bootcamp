89fc9c7c7856c040b2f6867e3804ff0d6c209614
Kyle P. Edwards <businesskylepedwards@outlook.com>
2025-06-05 13:26:12 -0500

    Updated - Added JavaScript

--- Front_End/html_css_and_js/js/script.js
+++ Front_End/html_css_and_js/js/script.js
@@ -1,1 +1,24 @@
 console.log("\nHello World.\n");
+
+var thisVariable = "Variable";
+let thisLet = 'Let';
+const thisConst = "Constant";
+
+// Single-line comment
+
+/*
+Multi-line comment 
+*/
+
+// Data Structures
+var str = "string";
+var num = 2;
+var flt = 3.14;
+var bool = true; // or "false"
+var list = ["1", 2, 3.14, ["Five"]];
+var thing = null;
+var notDefined = undefined;
+var getAge = function() { return 31; }; // function in a variabl
+var obj = {
+    'key': 'value pair'
+}; 
