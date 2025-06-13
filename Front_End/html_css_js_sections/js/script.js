cab974539cf16948a1a442e5c25fc938847fe92c
Kyle P. Edwards <businesskylepedwards@outlook.com>
2025-06-13 12:21:13 -0500

    Updated - played around with JavaScript

--- Front_End/html_css_js_sections/js/script.js
+++ Front_End/html_css_js_sections/js/script.js
@@ -44,3 +44,21 @@ var domManipulationPart2 = document.getElementById("DOM_manipulation_part_2");
 domManipulationPart1.innerText = "This text is 'innerText'.";
 domManipulationPart2.innerHTML = "This text is <strong>'innerHTML'</strong>.";
 
+// Array iteration
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// Old fashioned way:
+for (let i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+};
+
+// Modern way:
+arr.forEach(num => {console.log(num);});
+
+// Selecting multiple DOM nodes: document.querySelector(), document.querySelectorAll(), and so on.
+
+// Functions
+function firstFunction() {
+    console.log("\nFirst function call.\n");
+};
+
+firstFunction();
